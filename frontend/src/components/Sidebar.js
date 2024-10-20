d5712e550abab6da40d2c31998283d040ae15369
abhisheksh99 <abhishekleo1234@gmail.com>
2024-10-20 16:25:00 -0400

    Sidear component created

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,10 +1,68 @@
-import React from 'react'
-import "./myStyles.css"
+import React from 'react';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
+import "./myStyles.css";
+import ChatBodyItem from './ChatBodyItem';
 
 const Sidebar = () => {
-  return (
-    <div className="sidebar-container">Sidebar</div>
-  )
-}
+  const currentYear = new Date().getFullYear(); 
 
-export default Sidebar
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar-header">
+        <div>
+        <IconButton>
+          <AccountCircle />
+        </IconButton>
+
+        </div>
+
+        <div>
+        <IconButton>
+          <PersonAddIcon />
+        </IconButton>
+        <IconButton>
+          <GroupAddIcon />
+        </IconButton>
+        <IconButton>
+          <AddCircleIcon />
+        </IconButton>
+        <IconButton>
+          <NightlightIcon />
+        </IconButton>
+
+        </div>
+        
+        
+      </div>
+      <div className="sidebar-search">
+        <IconButton>
+        <SearchIcon />
+
+        </IconButton>
+        
+       
+        
+        <input placeholder='Search' className="search-box"/>
+
+        
+        
+      </div>
+      <div className="sidebar-body">
+        <ChatBodyItem/>
+
+
+      </div>
+      <div className="sidebar-footer">
+         QuickChat © {currentYear}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
