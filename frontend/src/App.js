83dcb2681baef65da0c6d3ae6df2c640e5ae01e2
abhisheksh99 <abhishekleo1234@gmail.com>
2024-10-20 15:43:21 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import MainContianer from "./components/MainContianer";
+
+function App() {
+  return (
+    <div className="App">
+     
+      <MainContianer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/WorkArea.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./myStyles.css"
+
+const WorkArea = () => {
+  return (
+    <div className="workarea-container">WorkArea</div>
+  )
+}
+
+export default WorkArea
