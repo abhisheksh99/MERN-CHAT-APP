202f9299f42d2e64d65d58c69d1195be08814f31
abhisheksh99 <abhishekleo1234@gmail.com>
2024-10-22 21:55:41 -0400

    Basic frontend and backend setup for project

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
+import {chats} from "./data/data.js"
 dotenv.config()
 
 const app = express();
 
+app.get("/", (req,res)=>{
+    res.send("Api is running")
+})
+
+app.get("/api/chats",(req,res)=>{
+    res.send(chats)
+})
+
+app.get("/api/chat/:id",(req,res)=>{
+    const singleChat = chats.find(c => c._id === req.params.id)
+    res.send(singleChat)
+    
+})
 
 app.listen(process.env.PORT,() => 
     console.log(`Server lsitening on ${process.env.PORT}`)
